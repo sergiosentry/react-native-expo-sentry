f750fa8186ab7e7a4937b964cafafa9beb7cb481
sergiosentry <109162568+sergiosentry@users.noreply.github.com>
2023-03-28 12:26:27 -0400

    specify transport

--- packages/telemetry/src/provider.ts
+++ packages/telemetry/src/provider.ts
@@ -2,17 +2,22 @@ import * as Sentry from "sentry-expo";
 
 import { createTelemetry } from "./telemetry";
 import { FeatureTag, Severity, TelemetryTags, TelemetryUser } from "./types";
+import  Constants from "expo-constants";
+import { makeFetchTransport } from "@sentry/browser";
 
 const MOBILE_DSN = "https://56ba5be9646a44109a51c0aa7175d28f@o87286.ingest.sentry.io/4504176016293888";
 
 const SentryProvider = {
+  
   init: () => {
+    console.log(Constants.executionEnvironment);
     Sentry.init({
       sampleRate: 1.0,
       dsn: MOBILE_DSN,
       enableInExpoDevelopment: true,
       debug: true,
       enableNative: true,
+      transport: Constants.executionEnvironment == "storeClient" ? makeFetchTransport : undefined
     });
   },
   captureError: (error: Error, tag: FeatureTag) => {
