d6e2326099e1d79f2be0eebb9003d628d529b21a
Kryštof Woldřich <31292499+krystofwoldrich@users.noreply.github.com>
2023-03-30 12:27:44 +0200

    Update packages/telemetry/src/provider.ts

--- packages/telemetry/src/provider.ts
+++ packages/telemetry/src/provider.ts
@@ -5,7 +5,7 @@ import { FeatureTag, Severity, TelemetryTags, TelemetryUser } from "./types";
 import  Constants from "expo-constants";
 import { makeFetchTransport } from "@sentry/browser";
 
-const MOBILE_DSN = "https://29fa9158fb09409da9c6d63f83f76332@o1357066.ingest.sentry.io/6643128";
+const MOBILE_DSN = "https://56ba5be9646a44109a51c0aa7175d28f@o87286.ingest.sentry.io/4504176016293888";
 
 const SentryProvider = {
   init: () => {
