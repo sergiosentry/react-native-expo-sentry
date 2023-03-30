75f93940ab53319210ee04bbfb8c953c39e797b9
Krystof Woldrich <krystofwoldrich@gmail.com>
2023-03-30 12:13:56 +0200

    Remove enable native as `sentry-expo` is handling this internally

--- packages/telemetry/src/provider.ts
+++ packages/telemetry/src/provider.ts
@@ -5,7 +5,7 @@ import { FeatureTag, Severity, TelemetryTags, TelemetryUser } from "./types";
 import  Constants from "expo-constants";
 import { makeFetchTransport } from "@sentry/browser";
 
-const MOBILE_DSN = "https://56ba5be9646a44109a51c0aa7175d28f@o87286.ingest.sentry.io/4504176016293888";
+const MOBILE_DSN = "https://29fa9158fb09409da9c6d63f83f76332@o1357066.ingest.sentry.io/6643128";
 
 const SentryProvider = {
   init: () => {
@@ -15,7 +15,6 @@ const SentryProvider = {
       dsn: MOBILE_DSN,
       enableInExpoDevelopment: true,
       debug: true,
-      enableNative: Constants.executionEnvironment == "storeClient" ? false : true,
       transport: Constants.executionEnvironment == "storeClient" ? makeFetchTransport : undefined
     });
   },
